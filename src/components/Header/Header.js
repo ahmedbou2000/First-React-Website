766ac6e6d1e8d2e156f1d82fe2fa7e66de6f567a
Bouguerba Ahmed <bouguerbaahmed06@gmail.com>
2023-07-18 20:41:15 +0200

    Ajouter le header navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,57 @@
 import { Component } from "react";
+import NavItem, { NavItemDropDown } from "../NavItem/NavItem";
 import './Header.css'
 
 class Header extends Component {
-    constructor(){
+    constructor() {
         super()
     }
 
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <a className="navbar-brand" href="#">React Website</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-    render(){
-        return(
-            <h1> Header </h1>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <NavItem >
+                                <a className="nav-link" href="#">Home</a>
+                            </NavItem>
+                            <NavItem >
+                                <a className="nav-link" href="#">Browse</a>
+                            </NavItem>
+
+                            <NavItemDropDown>
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Details
+                                </a>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a className="dropdown-item" href="#">Action</a>
+                                    <a className="dropdown-item" href="#">Another action</a>
+                                    <div className="dropdown-divider"></div>
+                                    <a className="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </NavItemDropDown>
+                            <NavItem >
+                                <a className="nav-link" href="#">Streams</a>
+                            </NavItem>
+                            <NavItem >
+                                <a className="nav-link" href="#">Profile</a>
+                            </NavItem>
+                        </ul>
+                        <form className="form-inline my-2 my-lg-0">
+                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
         )
-    }  
+    }
 }
 
 export default Header
